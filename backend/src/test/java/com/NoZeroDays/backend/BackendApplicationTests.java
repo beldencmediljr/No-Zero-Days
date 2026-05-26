5879cf27e44b6ff093a80f717ae009f9ee9afba1
unknown <ajlocco21@gmail.com>
2026-05-26 15:31:56 +0800

    folders

---
+++ backend/src/test/java/com/NoZeroDays/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.NoZeroDays.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
